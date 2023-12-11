db7174103d9fbbf347f16e5a726cbfcc3bec198d
Xeraserafinapat <marcopolo97@vp.pl>
2023-12-11 16:37:29 +0100

    implement solution for day 9 in one line of code

--- app/solutions/week2/solution9.ts
+++ app/solutions/week2/solution9.ts
@@ -46,6 +46,11 @@ function printSolution9Part2() {
   console.log(sum);
 }
 
+function printSolutions9InTwoLines() {
+  console.log(fs.readFileSync('./app/res/week2/input9.txt').toString().split('\n').map(line => line.split(" ").map(n => +n)).map(values => ([new Array(values.length-1).fill(0).map((_, i) => i === 0 ? values : _)].map(arr => new Array(values.length).fill(0).forEach((_, i) => arr[i] = arr[i - 1] !== undefined ? (arr[i - 1] as number[]).map((_, i, list) => list[i] - (list[i - 1] ?? 0)).slice(1) : arr[i]) as undefined ?? arr)[0] as number[][]).map(difs => difs[difs.length - 1]).reduce((acc, d) => acc + d)).reduce((acc, n) => acc + n));
+  console.log(fs.readFileSync('./app/res/week2/input9.txt').toString().split('\n').map(line => line.split(" ").map(n => +n)).map(values => values.reverse()).map(values => ([new Array(values.length-1).fill(0).map((_, i) => i === 0 ? values : _)].map(arr => new Array(values.length).fill(0).forEach((_, i) => arr[i] = arr[i - 1] !== undefined ? (arr[i - 1] as number[]).map((_, i, list) => list[i] - (list[i - 1] ?? 0)).slice(1) : arr[i]) as undefined ?? arr)[0] as number[][]).map(difs => difs[difs.length - 1]).reduce((acc, d) => acc + d)).reduce((acc, n) => acc + n));
+}
+
 export function printSolutions9() {
   printSolution9Part1();
   printSolution9Part2();
